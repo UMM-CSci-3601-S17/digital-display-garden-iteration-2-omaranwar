fe1dbd4e1c9fd882dbb142dadb90ddb44c5ec28d
antin006 <antin006@morris.umn.edu>
2017-03-23 23:06:53 -0500

    Format and populate plant and bed lists
    The plant list part of the page has been populated with all of the plants.
    The bed list contains 13 (12 viewable) numbers that can be selected to
    send a message to the console. Filter will be implemented later on.
    
    Address issue #2

--- client/src/app/plants/plant-list.component.ts
+++ client/src/app/plants/plant-list.component.ts
@@ -13,8 +13,12 @@ export class PlantListComponent implements OnInit {
     public plants: Plant[];
     public locations: Plant[];
 
+    // Don't filter if already filtered
+    public currentBedFilter;
+
     constructor(private plantListService: PlantListService) {
         // this.plants = this.plantListService.getPlants();
+        //this.locations = this.plants;
     }
 
     public getSelectedBed(): string{
@@ -26,27 +30,58 @@ export class PlantListComponent implements OnInit {
         var bed = this.getSelectedBed();
         var filterUrl = "?gardenLocation=" + bed;
 
-        this.plantListService.getFlowersByFilter(filterUrl).subscribe (
-            plants => this.plants = plants,
-            err => {
-                console.log(err);
-            }
-        );
-    }
-
-    ngOnInit(): void {
-        // this.plantListService.getPlants().subscribe(
+        // this.plantListService.getFlowersByFilter(filterUrl).subscribe (
         //     plants => this.plants = plants,
         //     err => {
         //         console.log(err);
         //     }
         // );
+    }
+    public handleClick(bedNumber): void{
+        console.log("Checking Bed number " + bedNumber );
 
-        this.plantListService.getGardenLocations().subscribe(
-            locations => this.locations = locations,
+        // if(this.currentBedFilter != bedNumber) {
+        //     this.currentBedFilter = bedNumber;
+        //     this.filterByBedNumber(bedNumber);
+        // }
+    }
+
+    public filterByBedNumber(bedNumber: string): void{
+
+        // if("13" == bedNumber)
+        //     console.log("String 13 === " + bedNumber);
+        //
+        // if(13 === bedNumber)
+        //     console.log("Value 13 === " + bedNumber);
+
+        console.log("Bed number " + bedNumber );
+
+        this.plants.forEach((plant, index) => {
+            console.log("Checking: " + plant.gardenLocation + " === " + bedNumber);
+           if(plant.gardenLocation == bedNumber) {
+               console.log("Added " + plant.commonName + " to [" + index + "]");
+               this.locations[index] = new Plant(plant.commonName);
+           }
+        });
+
+        // this.locations.forEach((plant, index) => {
+        //     console.log(plant.commonName + " " + plant.cultivar);
+        // });
+    }
+
+    ngOnInit(): void {
+        this.plantListService.getPlants().subscribe(
+            plants => this.plants = plants,
             err => {
                 console.log(err);
             }
         );
+
+        // this.plantListService.getGardenLocations().subscribe(
+        //     locations => this.locations = locations,
+        //     err => {
+        //         console.log(err);
+        //     }
+        // );
     }
 }
--- client/src/app/plants/plant.ts
+++ client/src/app/plants/plant.ts
@@ -1,13 +1,18 @@
-export interface Plant {
-    id: string,
-    plantID: string,
-    plantType: string,
-    commonName: string,
-    cultivar: string,
-    source: string,
+export class Plant {
+    id: string;
+    plantID: string;
+    plantType: string;
+    commonName: string;
+    cultivar: string;
+    source: string;
     gardenLocation: string;
-    year: number,
-    pageURL: string,
-    plantImageURLs: string[],
-    recognitions: string[]
+    year: number;
+    pageURL: string;
+    plantImageURLs: string[];
+    recognitions: string[];
+
+    constructor(commonName: string) {
+        this.commonName = commonName;
+    }
 }
+
