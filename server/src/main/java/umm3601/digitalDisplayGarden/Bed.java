5992cd204fb70aaaec804c563712ffbf51e38f1c
Dogxx000 <scot0530@morris.umn.edu>
2017-03-29 22:18:52 -0500

    Sort beds using custom comparitor.

---
+++ server/src/main/java/umm3601/digitalDisplayGarden/Bed.java
@@ -0,0 +1,13 @@
+package umm3601.digitalDisplayGarden;
+
+import org.bson.types.ObjectId;
+import java.util.ArrayList;
+
+import static java.lang.Double.parseDouble;
+
+/**
+ * Created by Dogxx000 on 3/28/17.
+ */
+public class Bed {
+    String  _id;
+}
